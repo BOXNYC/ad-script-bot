c1852508edfef31b0475191e2dc4b8bac1634226
Joseph James Weitzel <joe@box.biz>
2022-11-07 22:40:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(bodyParser.urlencoded({ extended: false }));
+  .use(bodyParser.json());
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    if ( req.query ) console.log(req.query)
+    res.render('pages/index', {
+      query: req.query
+    })
+  )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
